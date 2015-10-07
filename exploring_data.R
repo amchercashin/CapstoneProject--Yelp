1cdbc22a53688ceffc1505508c91497bcc02fbed
amchercashin <amchercashin@gmail.com>
2015-10-07 19:18:13 +0300

    reading data

--- exploring_data.R
+++ exploring_data.R
@@ -1,7 +1,10 @@
 library(jsonlite)
 
+# Reading data
 business <- stream_in(file("./data/yelp_academic_dataset_business.json"))
 checkin <- stream_in(file("./data/yelp_academic_dataset_checkin.json"))
 review <- stream_in(file("./data/yelp_academic_dataset_review.json"))
 tip <- stream_in(file("./data/yelp_academic_dataset_tip.json"))
 user <- stream_in(file("./data/yelp_academic_dataset_user.json"))
+
+#Exploratory data analisis
