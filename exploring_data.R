6a4d9e56222eda18c68a1f5b3720f94b340e3f35
amchercashin <amchercashin@gmail.com>
2015-10-08 17:02:11 +0300

    friends counts of user friends

--- exploring_data.R
+++ exploring_data.R
@@ -8,3 +8,11 @@ tip <- stream_in(file("./data/yelp_academic_dataset_tip.json"))
 user <- stream_in(file("./data/yelp_academic_dataset_user.json"))
 
 #Exploratory data analisis
+sum(sapply(user$friends, length) >= 100)
+user100 <- user[sapply(user$friends, length) >= 100,]
+
+user100$friendsCounts <- lapply(user100$friends, function(u) {
+        sapply(u[[1]], function(f){
+                length(user$friends[user$user_id == f][[1]])
+        })
+})
